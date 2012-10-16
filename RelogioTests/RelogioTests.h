65276cf2f87755b97b0bce9054f3c47dd339994a
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-10-16 09:46:07 -0300

    Initial Commit

---
+++ RelogioTests/RelogioTests.h
@@ -0,0 +1,13 @@
+//
+//  RelogioTests.h
+//  RelogioTests
+//
+//  Created by Rafael Brigagão Paulino on 16/10/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RelogioTests : SenTestCase
+
+@end
---
+++ RelogioTests/RelogioTests.m
@@ -0,0 +1,32 @@
+//
+//  RelogioTests.m
+//  RelogioTests
+//
+//  Created by Rafael Brigagão Paulino on 16/10/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "RelogioTests.h"
+
+@implementation RelogioTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RelogioTests");
+}
+
+@end
